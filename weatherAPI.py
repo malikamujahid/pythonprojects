c179f924da41e001affcc27bdae5109e21a0bd64
MalikaMujahid~ <malikamujahid>
2025-07-08 19:13:18 +0500

    weatherchanges

--- weatherAPI.py
+++ weatherAPI.py
@@ -27,6 +27,7 @@ def descriptionWeather(cityWeather, cityname):
     print(f"The temperature in {cityname} is {temp}°C\n"
           f"The humidity in {cityname} is {humidity}%\n"
           f"Weather description: {description}")
+    logging.info("weather data dispolayed")
 
 descriptionWeather(cityWeather, cityname )
 
